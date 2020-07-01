02d9300ef30c6b8dedb9d2a10b46f25f33dd75a9
Chris Wong <wc38.wong@gmail.com>
2020-07-01 13:35:55 +0800

    Deploy to Heroku

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
     "fmt"
     "net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
     })
 
-    http.ListenAndServe(":8080", nil)
+    http.ListenAndServe(":" + port, nil)
 }
